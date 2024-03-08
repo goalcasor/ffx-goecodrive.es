2c0085bfdd907116b588739bc5a82f1739ba915f
andresc4f <91336528+agomezneo@users.noreply.github.com>
2024-03-08 16:41:22 +0100

    Update SectionThree.tsx

--- src/components/home-sections/SectionThree.tsx
+++ src/components/home-sections/SectionThree.tsx
@@ -41,10 +41,10 @@ export default function SectionThree() {
                     spaceBetween={30}
                     modules={[Autoplay, Pagination, Navigation]}
                     onAutoplayTimeLeft={onAutoplayTimeLeft}
-                    autoplay={{
+                   /*  autoplay={{
                         delay: 2000,
                         disableOnInteraction: false,
-                    }}
+                    }} */
                     className="mySwiper"
                 >
                     {ProductData.map(product => (
